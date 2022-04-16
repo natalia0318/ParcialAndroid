0e8324f97c125df734542e54f6e8de070cc1d394
natalia0318 <nataliacuervocastano@gmail.com>
2022-04-15 20:52:54 -0500

    vistas terminadas, creacion funcionalidad del boton imc de la vista principal.

---
+++ parcialimc2/app/src/main/java/com/nc/parcialimc/Imc.java
@@ -0,0 +1,14 @@
+package com.nc.parcialimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Imc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
---
+++ parcialimc2/app/src/main/java/com/nc/parcialimc/Temperatura.java
@@ -0,0 +1,14 @@
+package com.nc.parcialimc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Temperatura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperatura);
+    }
+}
